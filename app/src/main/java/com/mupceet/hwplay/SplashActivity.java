077c23990fe9ea3f8827219164923faf5aadfab0
laiguizhong <laiguizhong@gmail.com>
2018-01-01 11:55:29 +0800

    Use EasyPermissions
    原生的权限申请写起来很别扭，使用Google提供的
    EasyPermissions（Google都看不下去……）

--- app/src/main/java/com/mupceet/hwplay/SplashActivity.java
+++ app/src/main/java/com/mupceet/hwplay/SplashActivity.java
@@ -1,23 +1,23 @@
 package com.mupceet.hwplay;
 
 import android.Manifest;
-import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.PermissionChecker;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
 
+import java.util.List;
+
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import pub.devrel.easypermissions.EasyPermissions;
 
-public class SplashActivity extends AppCompatActivity {
+public class SplashActivity extends AppCompatActivity implements EasyPermissions.PermissionCallbacks {
 
     private static final int PERMISSION_REQUEST_CODE_STORAGE = 1;
-    private String[] PERMISSION_STORAGE = {
+    private final String[] PERMISSION_STORAGE = {
             Manifest.permission.WRITE_EXTERNAL_STORAGE,
             Manifest.permission.READ_EXTERNAL_STORAGE
     };
@@ -43,32 +43,37 @@ public class SplashActivity extends AppCompatActivity {
     @OnClick(R.id.button_enter)
     public void goHome() {
         sp.edit().putBoolean(Constant.SP_IS_FIRST, false).apply();
-        verifyStoragePermission(this);
+        goHomeActivityWithCheckPermission();
     }
 
-    private void verifyStoragePermission(Activity activity) {
-        int checkPermission = ActivityCompat.checkSelfPermission(activity,
-                Manifest.permission.WRITE_EXTERNAL_STORAGE);
-
-        if (checkPermission == PermissionChecker.PERMISSION_GRANTED) {
+    private void goHomeActivityWithCheckPermission() {
+        if (EasyPermissions.hasPermissions(this, PERMISSION_STORAGE)) {
             goHomeActivity();
         } else {
-            ActivityCompat.requestPermissions(activity,
-                    PERMISSION_STORAGE, PERMISSION_REQUEST_CODE_STORAGE);
+            EasyPermissions.requestPermissions(this, null,
+                    PERMISSION_REQUEST_CODE_STORAGE, PERMISSION_STORAGE);
         }
+    }
+
+    @Override
+    public void onPermissionsGranted(int requestCode, @NonNull List<String> list) {
+        // 申请权限成功
+        Toast.makeText(this, "授权SD卡权限成功", Toast.LENGTH_SHORT).show();
+        goHomeActivity();
+    }
+
+    @Override
+    public void onPermissionsDenied(int requestCode, @NonNull List<String> list) {
+        // 申请权限失败
+        Toast.makeText(this, "授权SD卡权限失败，可能会影响应用的使用",
+                Toast.LENGTH_SHORT).show();
+        goHomeActivity();
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        // 因为我们只申请了一组权限，就不需要使用 requestCode 进行区分了
-        if (grantResults[0] == PermissionChecker.PERMISSION_GRANTED) {
-            // 申请权限成功
-            Toast.makeText(this, "授权SD卡权限成功", Toast.LENGTH_SHORT).show();
-        } else {
-            // 申请权限失败
-            Toast.makeText(this, "授权SD卡权限失败，可能会影响应用的使用", Toast.LENGTH_SHORT).show();
-        }
-        goHomeActivity();
+        // Forward results to EasyPermissions
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this);
     }
 }
